ad23c62212e7b9ad228e2ff24a9524abc4e06c0a
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-03-26 11:59:29 +0100

    mongo db new conection cloud.mongodb.com
    mongo db new conection cloud.mongodb.com

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 
 module.exports = {
-  development: 'mongodb://154.127.179.14:5000/sms_go',
-  test_evennode: 'mongodb://154.127.179.14:5000/sms_go',
+  
+  development: 'mongodb+srv://dbUser:dbUserPassword@cluster0-txvil.mongodb.net/test?retryWrites=true&w=majority',
+  //development: 'mongodb://154.127.179.14:5000/sms_go',
+  test_evennode: 'mongodb+srv://dbUser:dbUserPassword@cluster0-txvil.mongodb.net/test?retryWrites=true&w=majority',
   production: 'nonde'
 }
