58a307805d9cdd22aff3759ba56359ea27f68ae6
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-04-27 17:43:27 +0100

    database config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  development: 'mongodb+srv://dbUser:dbUserPassword@cluster0-txvil.mongodb.net/test?retryWrites=true&w=majority',
+  development: 'mongodb+srv://dbUser:dbUserPassword@cluster0-txvil.mongodb.net/sms_go?retryWrites=true&w=majority',
   //development: 'mongodb://154.127.179.14:5000/sms_go',
-  test_evennode: 'mongodb+srv://dbUser:dbUserPassword@cluster0-txvil.mongodb.net/test?retryWrites=true&w=majority',
+  test_evennode: 'mongodb+srv://dbUser:dbUserPassword@cluster0-txvil.mongodb.net/sms_go?retryWrites=true&w=majority',
   production: 'nonde'
 }
